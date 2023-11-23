089c14f77411e3ff8a9dba797899b610b5078310
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-11-23 16:17:24 +0000

    Add task 3

--- milestone_2.py
+++ milestone_2.py
@@ -8,14 +8,19 @@
 
 # Task 2: Choose a random word from the list
 
-import random
+# import random
 
 
-favorite_fruits=['Apple','Banana','Orange','Mango','Strawberry']
+# favorite_fruits=['Apple','Banana','Orange','Mango','Strawberry']
 
-word_list= favorite_fruits
+# word_list= favorite_fruits
 
-random_word = random.choice(word_list)
+# random_word = random.choice(word_list)
 
-word = random_word
-print(word)
+# word = random_word
+# print(word)
+
+# Task 3: Ask the user for an input
+
+geuss = input("Enter a single letters:")
+
