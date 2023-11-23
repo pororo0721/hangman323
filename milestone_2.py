ab8aef124e89d30c6c41da09305f2ad688fee480
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-11-23 16:13:46 +0000

    Add task 2

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,21 @@
+# Task 1: Define the list of possible words
+
+# favorite_fruits=['Apple','Banana','Orange','Mango','Strawberry']
+
+# word_list= favorite_fruits
+
+# print(word_list)
+
+# Task 2: Choose a random word from the list
+
+import random
+
+
 favorite_fruits=['Apple','Banana','Orange','Mango','Strawberry']
 
 word_list= favorite_fruits
 
-print(word_list)
+random_word = random.choice(word_list)
+
+word = random_word
+print(word)
