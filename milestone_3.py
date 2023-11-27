9aee5527b0d10ab3eacb3c13093e4dc263e0f266
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2023-11-27 12:07:44 +0000

    Add task 3

--- milestone_3.py
+++ milestone_3.py
@@ -23,18 +23,56 @@
 # For example, if the user guesses the letter "a" and the secret word is "apple", 
 # then your code should check if "a" is in "apple".
 
-secre_word="apple"
+# secre_word="apple"
 
-while True:
-    guess = input("Enter a single letters:")
+# while True:
+#     guess = input("Enter a single letters:")
 
     # Step 1: Check if the guess is in the word
-    if guess in secre_word:
+    # if guess in secre_word:
         # Step 2: if the guess is in the word, print a success message
-        print(f"Good guess! {guess} is in the word.")
-        break
-    else:
+    #     print(f"Good guess! {guess} is in the word.")
+    #     break
+    # else:
         # Step 3: If the guess is not in the word, print an error message
-        print(f"Sorry, {guess} is not in the word. Try again.")
+#         print(f"Sorry, {guess} is not in the word. Try again.")
 
-print("You guessed:", guess)           
+# print("You guessed:", guess)   
+
+#Task 3: Create functions to run the checks
+# Create 2 functions, check_guess and ask_for_input functions which contain 
+# the code for those two things.abs
+# The check_guess function will take the guessed letter as an argument and
+# check if the letter is in the word.
+
+secre_word= "apple"
+
+# Step 1: Define a function called check_guess. pass in the guess as a parameter for the function.
+def check_guess(guess):
+    # Step 2: Convert the guess into lower case.abs
+    guess = guess.lower()
+
+    # Step 3: Check if the guess is in the word
+    if guess in secre_word:
+        print(f"Good guess! {guess} is in the word.")
+        return True
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False    
+
+# Step 1: Define a function called ask_for_input.
+def ask_for_input():
+    while True:
+        # Step 2: Move the code for getting a valid letter guess into this function.
+      guess= input("Guess a letter: ")
+
+    
+      if guess.isalpha() and len(guess) ==1:
+        # Step 3: Call the check_guess function to check if the guess is in the word. 
+            if check_guess(guess):
+                break
+      else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+# Step 4: Call the ask_for_input function to test your code
+ask_for_input() 
